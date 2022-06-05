e288b8e84d02e7167da50606a572669518e68367
Brett Kromkamp <brettkromkamp@gmail.com>
2022-06-05 13:16:25 +0200

    Refactor paths and add not-found logic

--- topicdbapi/routers/attributes.py
+++ topicdbapi/routers/attributes.py
@@ -7,21 +7,25 @@ from ..dependencies import get_store
 
 store = get_store()
 
-router = APIRouter(prefix="/attributes", tags=["attributes"], responses={404: {"description": "Not found"}})
+router = APIRouter(prefix="/maps/{map}/attributes", tags=["attributes"], responses={404: {"description": "Not found"}})
 
 
-@router.get("/{map}/{attribute_id}")
+@router.get("/{attribute_id}")
 async def get_attribute(map: int, attribute_id: str):
     attribute = await store.get_attribute(map, attribute_id)
+    if not attribute:
+        raise HTTPException(status_code=404, detail="Attribute not found")
     result = AttributeModel.from_orm(attribute)
 
     return result
 
 
-@router.get("/entity/{map}/{entity_id}")
+@router.get("/{entity_id}/entities")
 async def get_attributes(map: int, entity_id: str, scope: str = None, language: Language = None):
     result = []
     attributes = await store.get_attributes(map, entity_id, scope, language)
+    if not attributes:
+        raise HTTPException(status_code=404, detail="Attributes not found")
 
     for attribute in attributes:
         result.append(AttributeModel.from_orm(attribute))
